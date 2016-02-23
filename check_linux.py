9c8784c843542c8fa3fb879451484459fa980641
applicant1844244 <applicant1844244@gmail.com>
2016-02-23 18:26:45 +0530

    Update check_linux.py
    1) Added input from terminal instead of modifying the source code. Directly run the program and mention your credentials, frequency for checking, and system username
    2) Developed the so-called user interface

--- check_linux.py
+++ check_linux.py
@@ -1,22 +1,23 @@
-import cups
-from twill.commands import *
-import html2text
-import subprocess
-import time
+import cups #for printing
+from twill.commands import * #for retrieving data
+import html2text #for "as name suggests"
+import subprocess #for speaking out decisions aloud
+import time #for adding delays
+import getpass #to input password without echoing on terminal
 
 ##Emotional words
 acceptance = ['congratulat', 'enjoy', 'party']
 rejection = ['sorry', 'unfortunately', 'disappoint']
 
 ##function to login and save the html
-def retrieve():
+def retrieve(usrname, passwd):
+    
     go('https://decisions.mit.edu/decision.php')
 
-    fv("1", "username", "username") #replace with the actual username
-    fv("1", "password", "password") #replace with the actual password
+    fv("1", "username", usrname) 
+    fv("1", "password", passwd) 
     formaction('f','https://decisions.mit.edu/decision.php')
     submit()
-    
     save_html('decision.html')
 
 ##function to check if the applicant has been accepted
@@ -34,26 +35,39 @@ def check():
         return "Congratulations! You have been admitted to the class of 2020"
         #command to be spoken in case of acceptance
     elif any(x in converted for x in rejection):
-	return "I am extremely sorry. Unfortunately, you couldn't be admitted"
+        return "I am extremely sorry. Unfortunately, you couldn't be admitted"
         #command to be spoken in case of rejection
     else:
         print "Unable to identify"
-	return -1
+        return -1
 
 ##function to print the decision
-def printit():
+def printit(accname):
     conn = cups.Connection()
     printers = conn.getPrinters()
     printer_name = printers.keys()[1]
-    cups.setUser('username') #replace with the computer's account name
+    cups.setUser(accname)
     conn.printFile(printer_name, "decision.txt", "",{})
 
+print "-------------------------------------------"
+print "The awesomest way to check your MIT-ision!!"
+time.sleep(1)
+print "-------------------------------------------"
+print "Enter your credentials"
+usrname=raw_input("Username: ")
+passwd=getpass.getpass()
+print "-------------------------------------------"
+accname=raw_input("Enter linux username: ")
+print "-------------------------------------------"
+frequency=float(raw_input("Enter the frequency for checking decisions (in seconds)"))
+print "---- DECISIONS ARE BEING CHECKED ----------"
+
 while True:
-    retrieve()
+    retrieve(usrname, passwd)
     command=check()
     if command!=-1:
-        printit()
+        printit(accname)
         subprocess.call(['speech-dispatcher'])        #start speech dispatcher
         subprocess.call(['spd-say', command])         #say the command
         break
-    time.sleep(15) #recheck the decision every 15 seconds
+    time.sleep(frequency)
